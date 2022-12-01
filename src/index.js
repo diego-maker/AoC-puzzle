5e4670c3dce156cf248e9fb97d1d8c46c11ff3a1
Diego roberto <droberto950@gmail.com>
2022-12-01 15:43:00 -0300

    ATT PUZZLE 1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,42 @@ import fs from 'fs';
 
 fs.readFile('./src/file/input1.txt','utf8', function(err,data){
 	//Enviando para o console o resultado da leitura
-	console.log(data);
+
+	let rows = [JSON.stringify(data)]; 
+	rows.map(x=>{
+
+	
+		var elves = (x.replaceAll('\\r\\n', " "));
+		 elves = (elves.replaceAll(' ', ','));
+		 elves =  (elves.replaceAll(',,','/'));
+		 elves =  (elves.replaceAll('"',''));
+		
+		 elves = elves.split("/");	
+		 let elve= [];
+
+		 for (let index = 0; index < elves.length; index++) {
+			elve[index]= elves[index].split(','); 
+			elve[index] = 	elve[index].map(y=>{
+					if(parseInt(y) != NaN){
+				return parseInt(y);
+
+					}
+			 })
+			 let soma = 0 ;
+			 for (let i = 0; i < elve[index].length; i++) {
+				soma += elve[index][i];
+			 }
+
+			 elve[index] = soma;
+		 }
+		
+		 let max = elve.reduce(function(a, b) {
+			return Math.max(a, b);
+		}, -Infinity);
+
+		 console.log(max);
+		 
+	});
+
 });
 
