370d8bda2954b79f5a2fb11f48ef638a5d0d4e92
Diego roberto <droberto950@gmail.com>
2022-12-07 10:51:36 -0300

    add comentários

--- src/day5_part1.js
+++ src/day5_part1.js
@@ -28,32 +28,31 @@ fs.readFile('./src/file/day5.txt','utf8', function(err,data){
    
   for (let index = 0; index < dataLog.length; index++) {
     
-    let move = dataLog[index].substring(4,dataLog[index].indexOf('from'))
-    let ind = dataLog[index].indexOf('from') ;
+    let move = dataLog[index].substring(4,dataLog[index].indexOf('from')) 
+    let ind = dataLog[index].indexOf('from') ; 
     ind = ind + 5;
 
-    let ind2 = dataLog[index].indexOf('to') ;
+    let ind2 = dataLog[index].indexOf('to') ; 
     ind2 = ind2 + 2;
    
     let from = dataLog[index].substring(ind,dataLog[index].indexOf('to'))
     let to = dataLog[index].substring(ind2,dataLog[index].length)
 
-    move = parseInt(move);
-    from =   parseInt(from)- 1;
-    to =  parseInt(to) - 1 ;
+    move = parseInt(move); // quantos numeros de caixas serão movidas de array para outro
+    from =   parseInt(from)- 1; // qual posição vai ser retirado essas caixas
+    to =  parseInt(to) - 1 ; // qual posição vai ser inserido essas caixas
     
  
  var fixBox = []
-    for (let ind2 = 0; ind2 < move; ind2++) {
+    for (let ind2 = 0; ind2 < move; ind2++) { //filtrando quais caixas saem do array boxes[from] 
       
       fixBox[ind2] = boxes[from][ind2]
     }
     
     let moving = move
-
-    fixBox = fixBox.reverse()
+    fixBox = fixBox.reverse() //invertendo ordem respeitando o padrão de fila 
     
-    for (let ind2 = 0; ind2 < boxes[to].length; ind2++, moving++) {
+    for (let ind2 = 0; ind2 < boxes[to].length; ind2++, moving++) { //caixas sendo inseridas no array boxes[to]
       
       fixBox[moving] = boxes[to][ind2]
       
@@ -64,15 +63,17 @@ fs.readFile('./src/file/day5.txt','utf8', function(err,data){
 
  
   }
-  console.log(boxes[0][0])
-  console.log(boxes[1][0])
-  console.log(boxes[2][0])
-  console.log(boxes[3][0])
-  console.log(boxes[4][0])
-  console.log(boxes[5][0])
-  console.log(boxes[6][0])
-  console.log(boxes[7][0])
-  console.log(boxes[8][0])
+  console.log(`
+  ${boxes[0][0]}${boxes[1][0]}${boxes[2][0]}${boxes[3][0]}${boxes[4][0]}${boxes[5][0]}${boxes[6][0]}${boxes[7][0]}
+  posição 1:${boxes[0][0]} 
+  posição 2:${boxes[1][0]} 
+  posição 3:${boxes[2][0]} 
+  posição 4:${boxes[3][0]} 
+  posição 5:${boxes[4][0]} 
+  posição 6:${boxes[5][0]} 
+  posição 7:${boxes[6][0]} 
+  posição 8:${boxes[7][0]} 
+  posição 9:${boxes[8][0]}`)
 
 
   })
