4394ae74de8edc9919a8035c2722c1700ffbf76c
diego-make <droberto950@gmail.com>
2022-12-29 14:21:39 -0300

    add day8

---
+++ src/day8.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+
+fs.readFile('./src/file/day7.txt','utf8', function(err,data){
+	//Enviando para o console o resultado da leitura
+
+	let rows = [JSON.stringify(data)]; 
+	rows.map(x=>{
+
+
+  })
+})
